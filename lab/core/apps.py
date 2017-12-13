a7ecb3d068141a2061e12befd2b8104e546cdaab
eltonplima <eltonplima@gmail.com>
2017-12-13 14:33:04 -0300

    Removed dynamic injection of custom user model.

--- lab/core/apps.py
+++ lab/core/apps.py
@@ -6,7 +6,3 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = 'lab.core'
-
-    def ready(self):
-        from django.conf import settings
-        settings.AUTH_USER_MODEL = 'core.CustomUser'
