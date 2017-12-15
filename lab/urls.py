01f67e9d08aeab27bf063e674bfdcb50e48b2ee3
eltonplima <eltonplima@gmail.com>
2017-12-15 06:14:12 -0300

    Fix import error.

--- lab/urls.py
+++ lab/urls.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.conf.urls import url
 
-from lab.core.views import CoreView
+from lab.views import CoreView
 
 urlpatterns = [
     url(r'^$', CoreView.as_view(), name='lab.core'),
