87878a0f00e2ecbec57005feaf3c07cd197f4d27
eltonplima <eltonplima@gmail.com>
2017-12-15 02:06:42 -0300

    Implemented dynamic injection of our custom manager.

--- lab/apps.py
+++ lab/apps.py
@@ -2,23 +2,27 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
-from django.db import models
-from django.utils.datastructures import ImmutableList
+from django.apps import apps
+from django.db.models import Manager
 
 
 class LabConfig(AppConfig):
     name = 'lab'
 
     def ready(self):
-        from temba.orgs.models import Org
-
-        class BlackMage(models.Manager):
+        """
+        Dynamic injection of our custom model(BlackMage).
+        """
+        class BlackMage(Manager):
 
             def get_queryset(self):
-                print('BlackMage.get_queryset')
+                model_name = '%s.%s' % (self.model._meta.app_label, self.model.__name__)
+                print('BlackMage.get_queryset for %s' % model_name)
                 qs = super(BlackMage, self).get_queryset()
                 return qs
 
-        manager = BlackMage()
-        manager.model = Org
-        Org.objects = manager
+        models = (model for model in apps.get_models(True) if 'temba.' in model.__module__)
+        for model in models:
+            manager = BlackMage()
+            manager.model = model
+            model.objects = manager
