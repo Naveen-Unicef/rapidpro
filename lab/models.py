006fad72d4ea8f244b64c091a7f8b587066ec13f
eltonplima <eltonplima@gmail.com>
2017-12-18 23:03:27 -0300

    First approach to test.

--- lab/models.py
+++ lab/models.py
@@ -1,82 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf import settings
-from django.contrib.auth.models import Group, User
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
-from django.db import connection
 from django.db import models
-from django.db.models.signals import post_delete, post_save
-from django.dispatch.dispatcher import receiver
+from django.db import transaction
+from django.utils import timezone
 
-from lab.utils import database_user_exists
-from lab.utils import username_to_database
+from temba.channels.models import Channel
 from temba.contacts.models import Contact
-from temba.flows.models import Flow
 from temba.orgs.models import Org
-
-INDIA_STATES = (('AP', 'Andhra Pradesh'),
-                ('AR', 'Arunachal Pradesh'),
-                ('AS', 'Assam'),
-                ('BR', 'Bihar'),
-                ('CG', 'Chhattisgarh'),
-                ('GA', 'Goa'),
-                ('GJ', 'Gujarat'),
-                ('HR', 'Haryana'),
-                ('HP', 'Himachal Pradesh'),
-                ('JK', 'Jammu and Kashmir'),
-                ('JH', 'Jharkhand'),
-                ('KA', 'Karnataka'),
-                ('KL', 'Kerala'),
-                ('MP', 'Madhya Pradesh'),
-                ('MH', 'Maharashtra'),
-                ('MN', 'Manipur'),
-                ('ML', 'Meghalaya'),
-                ('MZ', 'Mizoram'),
-                ('NL', 'Nagaland'),
-                ('OR', 'Orissa'),
-                ('PB', 'Punjab'),
-                ('RJ', 'Rajasthan'),
-                ('SK', 'Sikkim'),
-                ('TN', 'Tamil Nadu'),
-                ('TR', 'Tripura'),
-                ('UK', 'Uttarakhand'),
-                ('UP', 'Uttar Pradesh'),
-                ('WB', 'West Bengal'),
-                ('TN', 'Tamil Nadu'),
-                ('TR', 'Tripura'),
-                ('AN', 'Andaman and Nicobar Islands'),
-                ('CH', 'Chandigarh'),
-                ('DH', 'Dadra and Nagar Haveli'),
-                ('DD', 'Daman and Diu'),
-                ('DL', 'Delhi'),
-                ('LD', 'Lakshadweep'),
-                ('PY', 'Pondicherry'),
-                )
-
-class UserState(models.Model):
-    """
-    Essa classe precisa ser reformulada.
-
-    Um usuário pode ser associado a mais de um estado.
-    """
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='state')
-    state = models.CharField(choices=INDIA_STATES, max_length=2)
-
-    def __str__(self):
-        return '{}({})'.format(self.get_state_display(), self.state)
-
-
-class GroupAssociation(models.Model):
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-    content_type = models.ForeignKey(ContentType,
-                                     on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey()
-
-    def __str__(self):
-        return '{}({})'.format(self.group.name, self.content_type.model)
 
 
 class ContactSecondaryOrg(models.Model):
@@ -87,58 +18,25 @@ class ContactSecondaryOrg(models.Model):
         return '{}.{}'.format(self.contact, self.org)
 
 
-###################################################################################################
-# Signals
-###################################################################################################
-
-# @receiver(post_save, sender=Org)
-# @receiver(post_save, sender=Flow)
-def create_group(sender, instance, **kwargs):
-    ct = ContentType.objects.get_for_model(sender)
-    state = instance.created_by.state
-    group_name = str(state)
-    group, _ = Group.objects.get_or_create(name=group_name,
-                                           defaults={'name': group_name})
-    GroupAssociation.objects.get_or_create(group=group,
-                                           content_type=ct,
-                                           object_id=instance.id,
-                                           defaults={
-                                               'group': group,
-                                               'content_object': instance
-                                           })
-
-
-# @receiver(post_save, sender=User)
-def create_database_role(instance, **kwargs):
-    """
-    Create a new postgres rule for Django user.
-    """
-    database_username = username_to_database(instance.username)
-
-    username, password = database_username, settings.DATABASES['default']['PASSWORD']
-
-    grant_commands = (
-        "GRANT ALL ON ALL TABLES IN SCHEMA public TO %s",
-        "GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO %s"
-    )
-    if not database_user_exists(username):
-        print('creating database role: %s' % database_username)
-        with connection.cursor() as cursor:
-            create_user_sql = "CREATE USER %s WITH ENCRYPTED PASSWORD %%s" % username
-            cursor.execute(create_user_sql, [password, ])
-            for grant_sql in grant_commands:
-                grant_sql = grant_sql % username
-                cursor.execute(grant_sql)
-
-
-@receiver(post_delete, sender=User)
-def drop_database_role(instance, **kwargs):
-    """
-    Drop postgres rule when Django user is delete.
-    """
-    print('drop user %s' % instance.username)
-    database_username = username_to_database(instance.username)
-    if database_user_exists(database_username):
-        with connection.cursor() as cursor:
-            sql = "DROP USER %s" % database_username
-            cursor.execute(sql)
+def move_contact_to_org(contact, new_org):
+    import uuid
+    with transaction.atomic():
+        original_org = contact.org
+        ContactSecondaryOrg.objects.create(contact=contact, org=new_org)
+        for channel in original_org.channels.all():
+            channel_name = '{} - {}'.format(channel.name, new_org.name)
+            if not Channel.objects.filter(name=channel_name, org=new_org).exists():
+                channel.pk = None
+                channel.uuid = uuid.uuid4()
+                channel.org = new_org
+                channel.name = channel_name
+                channel.created_at = timezone.datetime.now()
+                channel.modified_at = timezone.datetime.now()
+                channel.save()
+            # new_org.channels.add(channel)
+        for contact_group in contact.all_groups.filter(org=original_org):
+            contact_group.contacts.remove(contact)
+        all_contacts_group = new_org.all_groups.get(name__icontains='all')
+        all_contacts_group.contacts.add(contact)
+        contact.org = new_org
+        contact.save()
