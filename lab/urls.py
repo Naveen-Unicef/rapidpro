e50338372ef66906c31c21da65eeda35025f5d02
eltonplima <eltonplima@gmail.com>
2017-12-29 09:10:58 -0300

    Implemented view to recover all orgs.

--- lab/urls.py
+++ lab/urls.py
@@ -1,9 +1,13 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from lab.views import CoreView
+from lab.views import OrgView
 
 urlpatterns = [
+    url(r'^orgs$', OrgView.as_view(), name='lab.orgs'),
     url(r'^$', CoreView.as_view(), name='lab.core'),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'api'])
--- lab/views.py
+++ lab/views.py
@@ -1,8 +1,44 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.db.models import Q
 from django.views.generic import TemplateView
+from rest_framework import status
+from rest_framework.response import Response
+
+from temba.api.v2.views import BaseAPIView
+from temba.orgs.models import Org
 
 
 class CoreView(TemplateView):
     template_name = 'core.html'
+
+
+class OrgView(BaseAPIView):
+    """
+    Return the orgs that can be used by action MoveToOrg.
+    """
+    permission = 'orgs.org_api'
+
+    def get(self, request, *args, **kwargs):
+        org = request.user.get_org()
+        parent_org = org if org.parent is None else org.parent
+        orgs = Org.objects.filter(Q(id=parent_org.pk) | Q(parent=parent_org)).values('id', 'name')
+        data = []
+
+        for org in orgs:
+            item = {}
+            for k, v in org.items():
+                item['uuid' if k == 'id' else k] = v
+            data.append(item)
+
+        return Response(data, status=status.HTTP_200_OK)
+
+    @classmethod
+    def get_read_explorer(cls):
+        return {
+            'method': "GET",
+            'title': "View Current Org",
+            'url': reverse('api.v2.org'),
+            'slug': 'org-read'
+        }
